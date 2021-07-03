df57807293ec5fa375fe8191d3f8b85549a192a0
chiakiiwasaki <chiaki103ki@gmail.com>
2021-07-03 09:26:44 +0000

    [Add]task4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "Follow Users"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Follower Users"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
