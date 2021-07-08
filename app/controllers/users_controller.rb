f8a9067dad7ffd2a1c3079853f3a8ce189286ce2
chiakiiwasaki <chiaki103ki@gmail.com>
2021-07-08 07:45:51 +0000

    [Finish]task7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,29 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    now = Time.current
+    start_date = now.beginning_of_day
+    end_date = now.end_of_day
+    @books_day = @books.where(created_at: start_date..end_date).count
+    yesterday_start_date = now.yesterday.beginning_of_day
+    yesterday_end_date = now.yesterday.end_of_day
+    @books_yesterday = @books.where(created_at: yesterday_start_date..yesterday_end_date).count
+    if (@books_day != 0) && (@books_yesterday != 0)
+      @difference = (@books_day.to_f/@books_yesterday.to_f).round(2)*100
+    else
+      @difference = "-"
+    end
+    start_week = now.beginning_of_week
+    end_week = now.end_of_week
+    @books_week = @books.where(created_at: start_week..end_week).count
+    prev_start_week = now.prev_week(:monday)
+    prev_end_week = now.prev_week(:sunday)
+    @books_prev_week = @books.where(created_at: prev_start_week..prev_end_week).count
+    if (@books_week != 0) && (@books_prev_week != 0)
+      @week_difference = (@books_week.to_f/@books_prev_week.to_f).round(2)*100
+    else
+      @week_difference = "-"
+    end
   end
 
   def index
